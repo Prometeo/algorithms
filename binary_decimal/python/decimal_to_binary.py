890057d808cd46a042603941f2e313a3cac4be29
Prometeo <xairi.valdivia@gmail.com>
2019-04-07 21:02:34 -0500

    Decimal to Binary in python

---
+++ binary_decimal/python/decimal_to_binary.py
@@ -0,0 +1,21 @@
+import sys
+
+''' Basic function to get the binary from a decimal number '''
+
+binary_repr = []
+
+
+def convert_to_binary():
+    decimal_number = int(sys.argv[1])
+    while decimal_number > 0:
+        reminder = decimal_number % 2
+        binary_repr.append(reminder)
+        decimal_number = decimal_number // 2
+
+    binary_repr.reverse()
+    return binary_repr
+
+
+if __name__ == '__main__':
+    result = convert_to_binary()
+    print(str(result))
